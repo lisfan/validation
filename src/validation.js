360038db6287011f99f5e32b6397716aede78b84
lisfan <goolisfan@gmail.com>
2017-12-15 10:58:43 +0800

    optimize: 优化jsdoc注释

--- src/validation.js
+++ src/validation.js
@@ -1,8 +1,19 @@
 /**
+ * @file 简易数据类型检测工具函数
+ * @author lisfan <goolisfan@gmail.com>
+ * @version 1.0.2
+ * @licence MIT
+ */
+
+/**
  * 返回数据类型
  *
+ * @since 1.0.0
+ *
  * @ignore
+ *
  * @param {*} value - 任意值
+ *
  * @returns {string}
  */
 const typeOf = function (value) {
@@ -10,11 +21,15 @@ const typeOf = function (value) {
 }
 
 /**
- * 返回类型检查函数
+ * 返回类型检查的封装函数
+ *
+ * @since 1.0.0
  *
  * @ignore
+ *
  * @param {string} type - 指定类型值
- * @returns {string}
+ *
+ * @returns {function}
  */
 const isType = function (type) {
   return function (value) {
@@ -29,9 +44,11 @@ export default {
   /**
    * 返回数据类型
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {string}
    */
   'typeof': typeOf,
@@ -39,27 +56,33 @@ export default {
   /**
    * 检测值是否为undefined
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isUndefined: isType('undefined'),
   /**
    * 检测值是否为null
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isNull: isType('null'),
   /**
    * 检测值是否为undefined或null
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isNil(value) {
@@ -68,27 +91,33 @@ export default {
   /**
    * 检测值是否为布尔值
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isBoolean: isType('boolean'),
   /**
    * 检测值是否为数字
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isNumber: isType('number'),
   /**
    * 检测值是否为NaN
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isNaN(value) {
@@ -97,36 +126,44 @@ export default {
   /**
    * 检测值是否为整型数字
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isInteger: Number.isInteger,
   /**
    * 检测值是否为安全整型数字
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isSafeInteger: Number.isSafeInteger,
   /**
    * 检测值是否为有限值
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isFinite: Number.isFinite,
   /**
    * 检测值是否为大于等于0的安全整数
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isLength(value) {
@@ -135,18 +172,22 @@ export default {
   /**
    * 检测值是否为字符串
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isString: isType('string'),
   /**
    * 检测值是否为数组
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isArray: isType('array'),
@@ -154,9 +195,11 @@ export default {
    * 检测值是否为类数组对象
    * 类对象应该不是 null ，且 typeof 的结果是 "object"。
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isArrayLikeObject(value) {
@@ -165,9 +208,11 @@ export default {
   /**
    * 检测值是否为类数组
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isArrayLike(value) {
@@ -177,9 +222,11 @@ export default {
    * 检测值是否为对象
    * 会检查 value 是否是 Object 的 language type。 (例如： arrays, functions, objects, regexes, new Number(0), 以及 new String(''))
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isObject(value) {
@@ -189,9 +236,11 @@ export default {
    * 检测值是否为类对象
    * 类对象应该不是 null 以及 typeof 的结果是 "object"。
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isObjectLike(value) {
@@ -200,9 +249,11 @@ export default {
   /**
    * 检测值是否为纯对象
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isPlainObject(value) {
@@ -211,46 +262,48 @@ export default {
   /**
    * 检测值是否为空：空字符串、空数组、空对象，其余情况均为true
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isEmpty(value) {
-    if (this.isString(value) && value.length > 0) {
-      return false
-    } else if (this.isArray(value) && value.length > 0) {
-      return false
-    } else if (this.isObject(value) && Object.keys(value).length > 0) {
-      return false
-    }
-
-    return true
+    return !(this.isString(value) && value.length > 0
+      || this.isArray(value) && value.length > 0
+      || this.isObject(value) && Object.keys(value).length > 0)
   },
   /**
    * 检测值是否为函数的arguments对象
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isArguments: isType('arguments'),
   /**
    * 检测值是否为函数
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isFunction: isType('function'),
   /**
    * 检测值是否为DOM元素
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isElement: isType('element'),
@@ -258,36 +311,44 @@ export default {
   /**
    * 检测值是否为Symbol
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isSymbol: isType('symbol'),
   /**
    * 检测值是否为Error对象
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isError: isType('error'),
   /**
    * 检测值是否为正则表达式
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isRegExp: isType('regexp'),
   /**
    * 检测值是否为日期对象
    *
-   * @memberOf Validation
    * @function
+   * @memberOf Validation
+   *
    * @param {*} value - 任意值
+   *
    * @returns {boolean}
    */
   isDate: isType('date'),
